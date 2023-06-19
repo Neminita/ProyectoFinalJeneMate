d3957949a91cf7450b396330405f6ecba8552fd8
Neminita <jeneby.lopez.cisternas@gmail.com>
2023-06-19 00:03:12 -0400

    Actualizacion de mi codigo

---
+++ ProyectoFinal.py
@@ -0,0 +1,36 @@
+asientos = [[(i * columnas + j + 1) for j in range(columnas)] for i in range(filas)]
+# Definicion de Funcion para crear el menú de asientos y sus arreglos
+def Masientos():
+    for fila in asientos:
+        print("|", end="")
+        for asiento in fila:
+            if asiento == "x":
+                print("   x", end="")
+            else:
+                print(f"{asiento:4}", end="")
+        print("|")
+
+
+# Datos de usuario para ingresar en normal.
+
+
+def UsuarioNormal():
+
+    global l_rut, l_ban, l_nom, l_tel, l_asiento
+    print("Ingrese sus datos correspondientes. ")
+    global RutUs, NomUs, TelefUs, BancoUs
+    RutUs = str(input("Ingrese su rut sin guíon ni puntos: "))
+    l_rut.append(RutUs)
+    NomUs = str(input("Ingrese su nombre completo: "))
+    l_nom.append(NomUs)
+    TelefUs = int(input("Ingrese su teléfono de contacto: "))
+    l_tel.append(TelefUs)
+    BancoUs = str(input("Ingrese el nombre de su banco: "))
+    l_ban.append(BancoUs)
+    if (
+        BancoUs == "BancoDuoc"
+        or BancoUs == "Bancoduoc"
+        or BancoUs == "bancoDuoc"
+        or BancoUs == "BancoDuoc"
+        or BancoUs == "BANCODUOC"
+        or BancoUs == "bancoduoc"
