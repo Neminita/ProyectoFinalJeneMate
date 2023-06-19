7b22ab4a9deff2d5c7791d0f57d6ba097e62567f
Neminita <jeneby.lopez.cisternas@gmail.com>
2023-06-19 00:17:43 -0400

    Cambios y actualizacion

--- ProyectoFinal.py
+++ ProyectoFinal.py
@@ -59,11 +59,8 @@ def UsuarioNormal():
         descuento = 0
         print("El valor de éste pasaje es:", (ANormal))
 
-# Datos de usuario para ingresar en normal.
-
-
-def UsuarioNormal():
-
+# Datos de usuario para ingresar en Vip.
+def UsuarioVip():
     global l_rut, l_ban, l_nom, l_tel, l_asiento
     print("Ingrese sus datos correspondientes. ")
     global RutUs, NomUs, TelefUs, BancoUs
@@ -84,23 +81,34 @@ def UsuarioNormal():
         or BancoUs == "bancoduoc"
 
     ):
-        descuento = (ANormal) * 0.15
-        print("El valor NORMAL de éste pasaje es:", (ANormal - descuento))
+        descuento = (AVip) * 0.15
+        print("El valor VIP de éste pasaje es:", (AVip - descuento))
     else:
         descuento = 0
         print("El valor de éste pasaje es:", (ANormal))
 
 
-# Datos de usuario para ingresar en Vip.
-def UsuarioVip():
-    global l_rut, l_ban, l_nom, l_tel, l_asiento
-    print("Ingrese sus datos correspondientes. ")
-    global RutUs, NomUs, TelefUs, BancoUs
-    RutUs = str(input("Ingrese su rut sin guíon ni puntos: "))
-    l_rut.append(RutUs)
-    NomUs = str(input("Ingrese su nombre completo: "))
-    l_nom.append(NomUs)
-    TelefUs = int(input("Ingrese su teléfono de contacto: "))
-    l_tel.append(TelefUs)
-    BancoUs = str(input("Ingrese el nombre de su banco: "))
-    l_ban.append(BancoUs)
+    # Primera interfaz de menú.
+
+def menu():
+    while True:
+        try:
+            cont = 0
+            print("Elija una opcion porfavor")
+            print("1. Ver asientos disponibles.")
+            print("2. Comprar asiento.")
+            print("3. Anular vuelo.")
+            print("4. Modificar datos.")
+            print("5. Salir.")
+            OpMenu = int(input())
+            if OpMenu == 1:
+                Masientos()
+            elif OpMenu == 2:
+                Masientos()
+                eleccion = int(input("Seleccione un asiento: "))
+                if eleccion in [asiento for fila in asientos for asiento in fila]:
+                    for i, fila in enumerate(asientos):
+                        for j, asiento in enumerate(fila):
+                            if asiento == eleccion:
+                                asientos[i][j] = "   X"
+                                break
